23c2f57382e4b1dc04405d12bd3c396bab34fa20
Gianluca-stack <gianluca.sciberras.20@um.edu.mt>
2024-07-21 16:05:43 +0200

    Tech Interview Question Completed

--- public/script.js
+++ public/script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Define the IP address of the server (replace with your IP address)
+    const ip_address = "192.168.4.130";
     // Define the variables
-    const apiUrl = 'http://localhost:3000/api/data';
+    const apiUrl = `http://${ip_address}:8000/api/data`;
     const dataTable = document.querySelector('#data-table tbody');
     const popup = document.querySelector('#popup');
     const closePopup = document.querySelector('#close-popup');
@@ -13,6 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const fetchData = async () => {
         const response = await fetch(apiUrl);
         const data = await response.json();
+        console.log('Data:', data);
         renderTable(data);
     }; 
 
@@ -26,8 +29,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 <td>${record.id}</td>
                 <td>${record.name}</td>
                 <td>
-                    <button class="edit-btn" data-id="${record.id}">Update</button>
-                    <button class="delete-btn" data-id="${record.id}">Delete</button>
+                    <div class="actions">
+                        <button class="edit-btn" data-id="${record.id}">Update</button>
+                        <button class="delete-btn" data-id="${record.id}">Delete</button>
+                    </div>
                 </td>
             `;
             dataTable.appendChild(row); // Append the row to the table
@@ -47,26 +52,55 @@ document.addEventListener('DOMContentLoaded', () => {
         recordForm.reset();
         recordIdInput.value = '';
         popup.classList.remove('hidden');
+        updateURL();
     };
 
     const openEditPopup = (e) => {
-        const id = e.target.dataset.id; // Get the id of the record
-        const record = Array.from(dataTable.children).find(row => row.children[0].textContent === id); // Find the record in the table
-        // If the record exists, populate the form and show the popup window
-        if (record) {
-            recordIdInput.value = id;
-            nameInput.value = record.children[1].textContent; // Populate the form with the record data
-            popup.classList.remove('hidden');
+        console.log("openEditPopup", e.target);
+        const id = e.target.dataset.id;
+    
+        if (id) {
+            console.log("Data to be updated with id:", id);
+            // Fetch the record from the server
+            fetch(`http://${ip_address}:8000/api/data/${id}`, {
+                method: 'GET'
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json(); // Parse the response as JSON
+            })
+            .then(record => { // Log the response
+                console.log('Data to be updated:', record);
+                recordIdInput.value = record.id;
+                nameInput.value = record.name;
+                popup.classList.remove('hidden');
+                updateURL(record.id);
+            })
+            .catch(error => console.error('Error:', error));
+    
+            // Update the record in the table
+            const record = Array.from(dataTable.children).find(row => row.children[0].textContent === id);
+            console.log('Record to be updated:', record);
+            if (record) {
+                recordIdInput.value = record.children[0].textContent;
+                nameInput.value = record.children[1].textContent;
+            }
+        } else {
+            console.error('ID is undefined');
         }
     };
 
     const closePopupWindow = () => {
         // Reset the form and hide the popup window
         popup.classList.add('hidden');
+        clearURL();
     };
 
     // Save the record to the server
     const saveRecord = async (e) => {
+        console.log("saveRecord");
         e.preventDefault();
         const id = recordIdInput.value;
         const name = nameInput.value;
@@ -87,35 +121,93 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
-    // Delete the record from the server
-    const deleteRecord = async (e) => {
+    function deleteRecord(e) {
         const id = e.target.dataset.id;
-        const response = await fetch(`${apiUrl}/${id}`, { method: 'DELETE' }); // Send a DELETE request to the server
+        if (id) {
+            console.log('Data to be deleted with id:', id);
+            // delete the record from the server
+            fetch(`http://${ip_address}:8000/api/data/${id}`, {
+                method: 'DELETE'
+            })
+            .then(response => response.json()) // Parse the response as JSON
+            .then(data => { // Log the response
+                console.log('Data Deletion Complete');
+                console.log(data);
+            })
+            .catch(error => console.error('Error:', error));
 
-        // If the response is successful, fetch the data from the server
-        if (response.ok) {
-            fetchData();
+            // delete the record from the table
+            const record = Array.from(dataTable.children).find(row => row.children[0].textContent === id);
+            console.log('Record to be deleted:', record);
+            if (record) {
+                dataTable.removeChild(record);
+            }
+        } else {
+            console.error('ID is undefined');
         }
-    };
+    }
 
     // Add event listeners to the add button and the popup close button
     addBtn.addEventListener('click', openAddPopup);
     closePopup.addEventListener('click', closePopupWindow);
+    // closepopup on touchingn the close button
+    closePopup.addEventListener('touchend', closePopupWindow);
     popup.addEventListener('click', (e) => {
         if (e.target === popup) {
             closePopupWindow();
         }
-    });
+    }); 
 
     // Add event listener to the escape key
     document.addEventListener('keydown', (e) => {
         if (e.key === 'Escape') {
             closePopupWindow();
+
         }
     });
 
+    // event listener for touching device screen to close popup
+    document.addEventListener('touchend', (e) => {
+        if (e.target === popup) {
+            closePopupWindow();
+        }
+    });
+
+    // Handle URL state management
+    const updateURL = (id = '') => {
+        const newUrl = id ? `${window.location.pathname}?recordId=${id}` : window.location.pathname; // Update the URL with the record ID
+        history.pushState({ recordId: id }, '', newUrl); // Update the URL without refreshing the page
+    };
+
+    const clearURL = () => {
+        history.pushState({}, '', window.location.pathname); // Clear the URL without refreshing the page
+    };
+
+    // Restore state based on URL when the page loads
+    const checkURL = () => {
+        const urlParams = new URLSearchParams(window.location.search); // Get the URL parameters
+        const recordId = urlParams.get('recordId'); // Get the record ID from the URL parameters
+        // If the record ID exists, open the edit popup window
+        if (recordId) {
+            openEditPopup({ target: { dataset: { id: recordId } } }); 
+        }
+    };
+
+    // Add event listener to the popstate event
+    window.onpopstate = function(event) {
+
+        // If the state exists and the record ID is defined, open the edit popup window
+        if (event.state && event.state.recordId) {
+            openEditPopup({ target: { dataset: { id: event.state.recordId } } }); 
+        } else {
+            closePopupWindow();
+        }
+    };
+
     // Add event listener to the form submit event
     recordForm.addEventListener('submit', saveRecord);
+
+    checkURL();
 
     fetchData();
 });
--- server/server.js
+++ server/server.js
@@ -1,35 +1,40 @@
 const express = require('express') // Import express
 const bodyParser = require('body-parser'); // to parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+const path = require('path');
 const cors = require('cors');  //to enable CORS with various options.
 
 const {v4: uuidv4} = require('uuid'); //to generate unique id for each user
 
 const app = express(); // Initialize express
-const PORT = 3000; // Port to run the server on
 
 app.use(bodyParser.json());
 app.use(cors());
-
+app.use(express.static('public'));
 let data = []; // Array to store the data
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    // url to access the index.html file
+});
 
 // Route to get all the data
 // ----------------------------------------------
-
 // Define route handler for GET requests to '/api/data'
 app.get('/api/data', (req, res) => {
     res.json(data); // Send the data as json
-    console.log("Data sent");
 });
 
-// Route to get a single data
-// ----------------------------------------------
-// Define route handler for GET requests to '/api/data/:id'
-
+// Route to get a specific record
 app.get('/api/data/:id', (req, res) => {
-    const {id} = req.params; // Get the id from the request params
+    const { id } = req.params;
+    console.log('GET request for ID:', id);
+    const record = data.find(item => item.id === id);
 
-    const record = data.find((d) => d.id === id); // Find the data in the data array
-    res.json(record); // Send the data as json
+    if (!record) {
+        return res.status(404).send({ error: 'No record found' });
+    }
+
+    res.status(200).json(record);
 });
 
 // Route to add a new data
@@ -45,36 +50,56 @@ app.post('/api/data', (req, res) => {
     data.push(record); // Add the new data to the data array
     res.status(201).json(record); // Send the new data as json
 
+    console.log("-------------------Data Insertion Complete----------------------");
     console.log("Data added with id: " + record.id);
+    console.log("----------------------------------------------------------------");
 });
 
 // Route to update a data
 // ----------------------------------------------
 // Define route handler for PUT requests to '/api/data/:id'
 
+// Route to update a record
 app.put('/api/data/:id', (req, res) => {
-    const {id} = req.params.id; // Get the id from the request params
-    const updateRecord = req.body; // Get the new data from the request body
+    console.log('PUT request for ID:', req.params.id);
+    const { id } = req.params;
+    const { name } = req.body;
 
-    data = data.map((d) => (d.id === id ? {...record, ...updateRecord}: d)); // Update the data
-    res.json(updateRecord); // Send the updated data as json
+    if (!id) {
+        return res.status(400).send({ error: 'ID is required' });
+    }
+
+    const record = data.find(item => item.id === id);
+    if (!record) {
+        return res.status(404).send({ error: 'No record found' });
+    }
+
+    record.name = name; // Update the name
+
+    res.status(200).json(record); // Send the updated data as JSON
+
+    console.log("-------------------Data Updation Complete----------------------");
+    console.log("Data updated with id: " + record.id);
+    console.log(data)
+    console.log("----------------------------------------------------------------");
 });
 
 // Route to delete a data
 // ----------------------------------------------
 
-// Define route handler for DELETE requests to '/api/data/:id'
+// Delete API
 app.delete('/api/data/:id', (req, res) => {
-    const {id} = req.params.id; // Get the id from the request params
+    const { id } = req.params;
+    if (!id) {
+        return res.status(400).send({ error: 'ID is required' });
+    }
 
-    data = data.filter((d) => d.id !== id); // Filter out the data to be deleted
-    res.json({message: 'Data deleted'}); // Send a success message
-
-    console.log("Data deleted with id: " + id);
+    data = data.filter(item => item.id !== id);
+    res.status(200).send({ message: 'Data deleted', id , data});
 });
 
 // Start the server
-
+const PORT = 8000; // Set the port
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
 });
